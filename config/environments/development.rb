569dc4695994d24d524a65d1e906beadde242b14
David Ladowitz <david@ladowitz.com>
2012-11-11 14:12:00 -0800

    added home and devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ MessageBoard::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #default settings from devise install
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
