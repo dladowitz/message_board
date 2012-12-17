c999260308326ea6ca0aafa01e486b978bc33937
David Ladowitz <david@ladowitz.com>
2012-12-16 20:57:06 -0800

    added bootstrap

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module MessageBoard
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Device instructions for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
